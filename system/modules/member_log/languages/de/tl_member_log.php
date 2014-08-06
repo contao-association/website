f3e130b8c31fc15720c9960bceca11a2017b6490
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-08-06 15:30:25 +0200

    Added german translation for member log

---
+++ system/modules/member_log/languages/de/tl_member_log.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * member_log extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2008-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-member_log
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_member_log']['text']      = array('Notiz', 'Bitte geben Sie eine Notiz ein.');
+$GLOBALS['TL_LANG']['tl_member_log']['dateAdded'] = array('Erstellungsdatum');
+$GLOBALS['TL_LANG']['tl_member_log']['user']      = array('Benutzer');
+$GLOBALS['TL_LANG']['tl_member_log']['type']      = array('Typ');
+$GLOBALS['TL_LANG']['tl_member_log']['data']      = array('Daten');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_member_log']['text_legend'] = 'Notiz-Text';
+
+
+/**
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_member_log']['type']['note']          = 'Notiz';
+$GLOBALS['TL_LANG']['tl_member_log']['type']['personal_data'] = 'Persönliche Daten';
+$GLOBALS['TL_LANG']['tl_member_log']['type']['registration']  = 'Registrierung';
+
+
+/**
+ * Labels
+ */
+$GLOBALS['TL_LANG']['tl_member_log']['label_personal_data_field'] = 'Feld';
+$GLOBALS['TL_LANG']['tl_member_log']['label_personal_data_old']   = 'Bisher';
+$GLOBALS['TL_LANG']['tl_member_log']['label_personal_data_new']   = 'Neu';
+$GLOBALS['TL_LANG']['tl_member_log']['label_user']                = '%s (ID: %s)';
+$GLOBALS['TL_LANG']['tl_member_log']['label_user_deleted']        = 'Anonym (ID: %s)';
+
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_member_log']['new']  = array('Neue Notiz', 'Eine neue Notiz erstellen');
+$GLOBALS['TL_LANG']['tl_member_log']['edit'] = array('Notiz bearbeiten', 'Notiz ID %s bearbeiten');
