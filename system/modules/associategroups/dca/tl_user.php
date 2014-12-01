8163674dd66a457d931cf0bf39d7d066011ab3fb
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-12-01 15:42:46 +0100

    Fixed link to sync users

--- system/modules/associategroups/dca/tl_user.php
+++ system/modules/associategroups/dca/tl_user.php
@@ -16,7 +16,7 @@ $GLOBALS['TL_DCA']['tl_user']['config']['onsubmit_callback'][] = array('Associat
 
 $GLOBALS['TL_DCA']['tl_user']['list']['global_operations']['sync_associate_groups'] = array(
 	'label'				=> &$GLOBALS['TL_LANG']['tl_user']['sync_associate_groups'],
-	'href'				=> 'act=sync_tl_user_to_group',
+	'href'				=> 'key=sync_tl_user_to_group',
 	'class'				=> 'sync_associate_groups header_sync_all',
 	'attributes'		=> 'onclick="Backend.getScrollOffset();"',
 );
