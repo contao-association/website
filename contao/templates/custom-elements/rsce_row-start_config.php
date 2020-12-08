5373d445c08717e03bd0f7319efe5734d0a325aa
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-12-08 15:41:49 +0100

    Added bootstram grid start/stop elements

---
+++ contao/templates/custom-elements/rsce_row-start_config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'label' => ['Bootstrap Row START'],
+    'types' => ['content'],
+    'contentCategory' => 'Grid',
+    'wrapper' => ['type' => 'start'],
+    'standardFields' => ['cssID']
+];
---
+++ contao/templates/custom-elements/rsce_row-stop_config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'label' => ['Bootstrap Row STOP'],
+    'types' => ['content'],
+    'contentCategory' => 'Grid',
+    'wrapper' => ['type' => 'stop'],
+    'standardFields' => ['cssID']
+];
