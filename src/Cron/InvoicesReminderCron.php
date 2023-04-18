c97c7ee195721391b05025036f5a8ba81b5534e9
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-04-18 14:11:05 +0200

    Lower the amount of invoice reminders

--- src/Cron/InvoicesReminderCron.php
+++ src/Cron/InvoicesReminderCron.php
@@ -11,7 +11,7 @@ use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\MemberModel;
 use NotificationCenter\Model\Notification;
 
-#[AsCronJob('0 9 * * 1')] // run every monday at 9am
+#[AsCronJob('0 9 1-7,15-21 * *')] // run job two weeks per month, PHP code will make sure it only sends on mondays
 class InvoicesReminderCron
 {
     use ErrorHandlingTrait;
@@ -24,6 +24,11 @@ class InvoicesReminderCron
 
     public function __invoke(): void
     {
+        // only send reminders on mondays
+        if (1 !== (int) date('w')) {
+            return;
+        }
+
         $this->framework->initialize();
 
         $notification = Notification::findByPk($this->overdueNotificationId);
@@ -33,9 +38,19 @@ class InvoicesReminderCron
             return;
         }
 
+        $minDueDate = new \DateTimeImmutable('-2 weeks');
+
         foreach ($this->cashctrl->getOverdueInvoices() as $order) {
             // Skip invoices that are set to "Gemahnt 1" or "Gemahnt 2"
             if (\in_array($order->getStatusId(), [40, 43], true)) {
+                continue;
+            }
+
+            $dueDate = clone $order->getDate();
+            $dueDate->add(new \DateInterval('P'.(int) $order->getDueDays().'D'));
+
+            if ($dueDate > $minDueDate) {
+                // only send reminder if the invoice is at least two weeks overdue
                 continue;
             }
 
